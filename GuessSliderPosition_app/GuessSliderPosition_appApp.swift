44dfd464f5f486a43e619f619fe5f4bdcd8a7414
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-11-09 22:19:48 +0300

    Initial Commit

---
+++ GuessSliderPosition_app/GuessSliderPosition_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessSliderPosition_appApp.swift
+//  GuessSliderPosition_app
+//
+//  Created by Константин Кириллов on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessSliderPosition_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
